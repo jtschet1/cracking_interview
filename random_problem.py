246913ffae057767538cba9d25c47f813dc9ab38
Johann Tschetter <jtschet1@yahoo.com>
2025-06-01 11:14:35 -0500

    problem 27.4

---
+++ random_problem.py
@@ -0,0 +1,14 @@
+import random
+
+problems = [
+    "26.1", "26.2", "26.3", 
+    "27.1", "27.2", "27.3", "27.4"
+]
+
+def get_random_problem(arr):
+    """
+    Returns a random problem from the predefined set of problems.
+    """
+    return random.choice(arr)
+
+print(get_random_problem(problems))
